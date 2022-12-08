b28afd323faae78e65af4559a04cfad14b809231
dbuildster <114748852+dbuildster@users.noreply.github.com>
2022-12-07 19:39:56 -0500

    Add files via upload

--- prototype/index.js
+++ prototype/index.js
@@ -6,6 +6,24 @@ var requestOptions = {
   redirect: 'follow',
   headers: myHeaders
 };
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateRandomSearch() {
+    const randomCharacter = characters.charAt(Math.floor(Math.random() * characters.length))
+    let randomSearch = '';
+    switch (Math.round(Math.random())) {
+      case 0:
+        randomSearch = randomCharacter + '%';
+        break;
+      case 1:
+        randomSearch = '%' + randomCharacter + '%';
+        break;
+      default:
+        randomSearch = '%' + randomCharacter;
+    }
+    return randomSearch;
+  }
+
 
 const api_url = "https://api.apilayer.com/spotify/artists?ids=0YC192cP3KPCRWx8zr8MfZ"
 
@@ -22,22 +40,33 @@ async function getapi(url) {
     
     show(data);
 }
-getapi(api_url);
-// Function to define innerHTML for HTML table
-function show(data) {
-    let tab = 
+
+function show(data){
+    const tab = [];
+    for(let i = 0; i<2; i++){
+        tab[i] = 
     `<tr>
           <th>Name</th>
           <th>Followers</th>
           <th>Popularity</th>
           <th>Type</th>
          </tr>`;
-         tab += `<tr> 
-         <td>${data.artists[0].name} </td>
-         <td>${data.artists[0].followers.total}</td>
-         <td>${data.artists[0].popularity}</td> 
-         <td>${data.artists[0].type}</td>          
+         tab[i] += `<tr> 
+         <td>${data.artists.items[i].name} </td>
+         <td>${data.artists.items[i].followers.total}</td>
+         <td>${data.artists.items[i].popularity}</td> 
+         <td>${data.artists.items[i].type}</td>          
      </tr>`;
-    // Setting innerHTML as tab variable
-    document.getElementById("name").innerHTML = tab;
+    }
+    Table1 = tab[0]
+    Table2 = tab[1]
+    document.getElementById("option1").innerHTML = Table1;
+    document.getElementById("option2").innerHTML = Table2;
+}
+
+//https://v1.nocodeapi.com/kiryn/spotify/puFGJfMsgiBixtEE/search?q=%r&type=artist&perPage=2
+function startGame(){
+    const api_call = "https://v1.nocodeapi.com/kiryn/spotify/puFGJfMsgiBixtEE/search?q=" + generateRandomSearch() + "&type=artist&perPage=2";
+    getapi(api_call);
+    
 }
