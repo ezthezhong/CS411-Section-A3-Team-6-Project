598782706510d4ce161196574c530753f2853a6e
Kiryn Denmead <kirynd@bu.edu>
2022-12-08 10:42:01 -0500

    general cleanup

--- higher-or-lower/src/App.js
+++ higher-or-lower/src/App.js
@@ -1,40 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import TodoList from './TodoList';
 import {useEffect, useState} from 'react';
 import axios from 'axios';
 
 function App() {
-
-
-  const CLIENT_ID = "3a6379d1417b4ff3af48dfe94c5419c6"
-  const REDIRECT_URI = "http://localhost:3000"
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-  const RESPONSE_TYPE = "token"
-
-  const [token, setToken] = useState("")
-  const [searchKey, setSearchKey] = useState("")
-  const [artists, setArtists] = useState([])
-
-  useEffect(() => {
-    const hash = window.location.hash
-    let token = window.localStorage.getItem("token")
-
-    if (!token && hash) {
-        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
-
-        window.location.hash = ""
-        window.localStorage.setItem("token", token)
-    }
-
-    setToken(token)
-
-  }, [])
-  
-  const logout = () => {
-    setToken("")
-    window.localStorage.removeItem("token")
-  }
 
   const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
@@ -53,8 +22,6 @@ function App() {
     }
     return randomSearch;
   }
-
-  const api_url = "https://api.apilayer.com/spotify/artists?ids=0YC192cP3KPCRWx8zr8MfZ"
 
   var myHeaders = new Headers();
 
@@ -81,16 +48,9 @@ function App() {
     for (let i = 0; i< 2; i++) {
       tab[i] = 
       `<tr>
-      <th>Name</th>
-      <th>Followers</th>
-      <th>Popularity</th>
-      <th>Type</th>
       </tr>`;
       tab[i] += `<tr>
       <td>${data.artists.items[i].name} </td>
-      <td>${data.artists.items[i].followers.total}</td>
-      <td>${data.artists.items[i].popularity}</td> 
-      <td>${data.artists.items[i].type}</td>
       </tr>`;
       // const six = tab[0];
     }
@@ -98,7 +58,7 @@ function App() {
     const seven = document.getElementById("option2").innerHTML = tab[1];
   }
   function startGame() {
-    const api_call = "https://v1.nocodeapi.com/kiryn/spotify/puFGJfMsgiBixtEE/search?q=" + generateRandomSearch() + "&type=artist&perPage=2";
+    const api_call = "https://v1.nocodeapi.com/kirynmissy/spotify/AwQbQHZlDSCGWmtd/search?q=" + generateRandomSearch() + "&type=artist&perPage=2";
     document.getElementById("option2").innerHTML = (getapi(api_call));
     document.getElementById("option1").innerHTML = (getapi(api_call));
   }
