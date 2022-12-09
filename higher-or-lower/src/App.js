3789db1150b484b2c8e3aadcc9bfea82ab45aec1
dbuildster <114748852+dbuildster@users.noreply.github.com>
2022-12-08 19:05:46 -0500

    Add files via upload

--- higher-or-lower/src/App.js
+++ higher-or-lower/src/App.js
@@ -31,17 +31,22 @@ function App() {
     headers: myHeaders
   };
 
+  var followers = new Array();
+
+  const api_call = "https://v1.nocodeapi.com/kirynmissy/spotify/AwQbQHZlDSCGWmtd/search?q=" + generateRandomSearch() + "&type=artist&perPage=2";
+  var data;
   async function getapi(url) {
     
     // Storing response
     const response = await fetch(url, requestOptions);
     
     // Storing data in form of JSON
-    var data = await response.json();
+    data = await response.json();
     console.log(data);
 
     show(data);
 }
+
 
   function show(data) {
     const tab = [];
@@ -53,18 +58,34 @@ function App() {
       <td>${data.artists.items[i].name} </td>
       </tr>`;
       // const six = tab[0];
+      followers.push(data.artists.items[i].followers.total);
     }
     const six = document.getElementById("option1").innerHTML = tab[0];
     const seven = document.getElementById("option2").innerHTML = tab[1];
   }
   function startGame() {
-    const api_call = "https://v1.nocodeapi.com/kirynmissy/spotify/AwQbQHZlDSCGWmtd/search?q=" + generateRandomSearch() + "&type=artist&perPage=2";
     document.getElementById("option2").innerHTML = (getapi(api_call));
     document.getElementById("option1").innerHTML = (getapi(api_call));
   }
 
+  var score = 0
+  function isLeftCorrect(){
+  
+    if(followers[0] > followers[1]){
+      document.getElementById("score").innerHTML = score+=1;
+    }
+  }
+
+  function isRightCorrect(){
+      if(followers[1] > followers[0]){
+        document.getElementById("score").innerHTML = score+=1;
+      }
+    
+  }
+
   return ( 
-    startGame()
+    startGame(), isLeftCorrect(), isRightCorrect()
+    
   )
 }
 
